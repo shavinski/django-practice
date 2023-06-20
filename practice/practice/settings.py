f78fa36b30b26a7b9d7ade20d968a1ab9b7c0607
Jakob <shavinski.jakob@gmail.com>
2023-06-20 12:44:07 -0700

    finsih part 4 tutorial

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': 'django_practice',
+        'USER': 'jakob',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
